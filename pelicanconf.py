e66bf23071577806d19a15ae3faa8c3cf32bd51c
Gaurav <gaurav.ssnl@gmail.com>
2021-01-12 22:39:25 +0530

    Update pelicanconf to include pages and About.

--- pelicanconf.py
+++ pelicanconf.py
@@ -37,7 +37,7 @@ RELATIVE_URLS = True
 # THEME = "notmyidea"
 # THEME = "simple"
 # CSS_FILE = "wide.css"
-THEME = "themes/elegant-master"
+# THEME = "themes/elegant-master"
 
 ARTICLE_PATHS = ['blog']
 ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
@@ -50,7 +50,7 @@ FOOTER_LINKS = (('Pelican', 'https://getpelican.com/'),
          ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
          ('You can modify those links in your config file', '#'),)
 
-DIRECT_TEMPLATES = ['404', 'search']
+# DIRECT_TEMPLATES = ['404', 'search']
 
 # Plugins and extensions
 MARKDOWN = {
@@ -75,9 +75,25 @@ PLUGINS = [
     "share_post",
     "tipue_search",
 ]
-# SITEMAP = {
-#     "format": "xml",
-#     "priorities": {"articles": 0.5, "indexes": 0.5, "pages": 0.5},
-#     "changefreqs": {"articles": "monthly", "indexes": "daily", "pages": "monthly"},
-# }
 
+SITEMAP = {
+    "format": "xml",
+    "priorities": {"articles": 0.5, "indexes": 0.5, "pages": 0.5},
+    "changefreqs": {"articles": "monthly", "indexes": "daily", "pages": "monthly"},
+}
+
+# Hide About
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_CATEGORIES_ON_MENU = True
+USE_FOLDER_AS_CATEGORY = False
+PAGE_PATHS = ['pages']
+# Add page titles here if you don't want them linked to automatically
+EXCLUDED_PAGES = ['Web Chat']
+
+# Additional main menue items
+# MENUITEMS = [
+#     ('<i class="fas fa-info-circle"></i> About', '/pages/about.html'),
+#     ('<i class="fas fa-file-powerpoint"></i> Presentations', '/pages/presentations.html'),
+#     ('<i class="fab fa-discord"></i> Discord Chat <i class="fas fa-external-link-alt"></i>', 'https://discord.gg/ch7TPCx'),
+#     ('<i class="fab fa-meetup"></i> Meetup Group <i class="fas fa-external-link-alt"></i>', 'https://www.meetup.com/Phoenix-Python-Meetup-Group/'),
+# ]
